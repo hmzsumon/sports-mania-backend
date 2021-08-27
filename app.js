d42c7c8b44bea175176057e5c481ba54deb74bc5
Zaklaria <zsumonn@gmail.com>
2021-08-27 14:18:40 +0600

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 require('dotenv').config();
 const db = require('./db/db');
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/leagues', leaguesRouter);
